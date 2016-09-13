ff80458d033d957a197f8e6a7ff9a88793c411e9
Andre Henrique Almeida de Oliveira <vntahal@andre.venturus.local>
2016-09-13 13:08:08 -0300

    continue

--- alura/node/casadocodigo/app.js
+++ alura/node/casadocodigo/app.js
@@ -1,7 +1,11 @@
 var app = require('./config/express')();
+var http = require('http').Server(app);
 var ip = 'localhost';
 var port = 3000;
+var io = require('socket.io')(http);
 
-app.listen(port, function(){
+app.set('io', io)
+
+http.listen(port, function(){
 	console.log('Server is running at http://' + ip + ':' + port + "/");
 });
--- alura/node/casadocodigo/config/express.js
+++ alura/node/casadocodigo/config/express.js
@@ -5,6 +5,7 @@ var expressValidator = require('express-validator');
 
 module.exports = function () {
 	var app = express();
+	app.use(express.static('./app/public'));
 	app.set('view engine', 'ejs');
 	app.set('views', './app/views');
 
@@ -15,7 +16,23 @@ module.exports = function () {
 
 	//req -> middlewareBodyParser -> middlewareAutenticacao -> funcao que trata a requisacao
 
+
 	load('routes', {cwd: 'app'}).then('infra').into(app);
+
+	app.use(function (req, res, next) {
+		res.status(404).render('erros/404');
+		next();
+	});
+
+	// Invocando um midlleware com quatro argumentos o primeiro eh o erro.
+	// Tem preferencia.
+	app.use(function (error, req, res, next) {
+		if (process.env.NODE_ENV == 'production') {
+			res.status(500).render('erros/500');
+			return;
+		}
+		next(error);
+	});
 
 	return app;
 }
