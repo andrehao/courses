a34bb29a308be2dcff06e0c32a17936008d31619
Andre Henrique Almeida de Oliveira <vntahal@venturus.org.br>
2017-01-13 15:41:07 -0200

    angular2

---
+++ alura/angular2/alurapic2/src/app/foto/foto.module.ts
@@ -0,0 +1,10 @@
+import { FotoComponent } from './foto.component';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+    declarations: [ FotoComponent ]
+    
+})
+export class FotoModule {
+
+}
