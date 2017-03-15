b4c79884124ef205fcf270c6ec7fbd4832d89515
Andre Henrique Almeida de Oliveira <andre.oliveira@venturus.org.br>
2017-03-15 12:28:39 -0300

    Angular 2

--- alura/angular2/alurapic2/src/app/app.component.ts
+++ alura/angular2/alurapic2/src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { Http, Response } from '@angular/http';
 import { Component } from '@angular/core';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  fotos: Object[] = [];
+
+  constructor(private http: Http) {
+    this.http.get('http://localhost:3000/v1/fotos')
+      .map((response: Response) => response.json())
+      .subscribe(
+      (data: Object[]) => {
+        console.log(data);
+      }
+    );
+
+  }
 }
